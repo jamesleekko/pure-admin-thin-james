36610f3995715f151382c7ee73c7ad1829b33a7c
jamesleekko <jamesleecode@gmail.com>
2023-03-23 01:29:52 +0800

    feat: cancel mock, add real api

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,4 +1,5 @@
 import { http } from "@/utils/http";
+import { baseUrlApi } from "./utils";
 
 export type UserResult = {
   success: boolean;
@@ -28,12 +29,24 @@ export type RefreshTokenResult = {
   };
 };
 
-/** 登录 */
+/** mock登录 */
+// export const getLogin = (data?: object) => {
+//   return http.request<UserResult>("post", "/login", { data });
+// };
+
+/** 后端登录 */
 export const getLogin = (data?: object) => {
-  return http.request<UserResult>("post", "/login", { data });
+  return http.request<any>("post", baseUrlApi("login"), { data });
 };
 
-/** 刷新token */
+// /** mock刷新token */
+// export const refreshTokenApi = (data?: object) => {
+//   return http.request<RefreshTokenResult>("post", "/refreshToken", { data });
+// };
+
+/** 后端刷新token */
 export const refreshTokenApi = (data?: object) => {
-  return http.request<RefreshTokenResult>("post", "/refreshToken", { data });
+  return http.request<RefreshTokenResult>("post", baseUrlApi("refreshToken"), {
+    data
+  });
 };
